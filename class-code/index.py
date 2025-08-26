eb7242bb66bd6d6e650dad7e9415bb9c44fe0d2e
omarakamal <omaraakamal@gmail.com>
2025-08-26 19:56:54 +0300

    update

---
+++ class-code/index.py
@@ -0,0 +1,15 @@
+from color50 import css
+
+color = css('yellow')
+
+print(color + "This String is colored now")
+
+print(css("red") + "WARNING")
+
+
+
+# Exercise 4:
+# create a new virtual environment call it cow-venv
+# go into the cow-venv environment
+# install pycowsay from the pyPi library
+# Read the documentation and print a cow saying "I understand venv now!"
