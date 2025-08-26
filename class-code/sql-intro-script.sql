92b2e4ae0806d021f7e477e6e518d7c6acd97d58
omarakamal <omaraakamal@gmail.com>
2025-08-26 10:21:12 +0300

    update

---
+++ class-code/sql-intro-script.sql
@@ -0,0 +1,10 @@
+-- Creating a new table in our database
+CREATE TABLE bands(
+-- columnName dataType constraint
+	name VARCHAR(255),
+	genre VARCHAR(255)
+);
+
+
+-- Query a table
+SELECT * FROM bands;
