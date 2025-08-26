333b52e968ff59c7cf0e10d514a0dadc37f11358
omarakamal <omaraakamal@gmail.com>
2025-08-26 10:38:15 +0300

    update

--- class-code/sql-intro-script.sql
+++ class-code/sql-intro-script.sql
@@ -1,6 +1,7 @@
 -- Creating a new table in our database
 CREATE TABLE bands(
 -- columnName dataType constraint
+	band_id INTEGER,
 	name VARCHAR(255),
 	genre VARCHAR(255)
 );
@@ -8,3 +9,7 @@ CREATE TABLE bands(
 
 -- Query a table
 SELECT * FROM bands;
+
+
+-- Delete a table
+DROP TABLE bands;
